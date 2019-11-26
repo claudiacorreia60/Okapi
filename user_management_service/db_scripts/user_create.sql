ea4082b810aff9aa022026a00169c775247b8c47
Rui Calheno <a78085@alunos.uminho.pt>
2019-11-26 12:27:47 +0000

    Change db user tables to english

--- user_management_service/db_scripts/user_create.sql
+++ user_management_service/db_scripts/user_create.sql
@@ -19,23 +19,23 @@ USE `okapi_users` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `okapi_users`.`user` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
   `username` VARCHAR(15) NOT NULL,
-  `sexo` VARCHAR(45) NOT NULL,
-  `data_nascimento` DATE NOT NULL,
-  `fotografia` VARCHAR(45) NULL,
+  `gender` VARCHAR(45) NOT NULL,
+  `birthday` DATE NOT NULL,
+  `photo` VARCHAR(45) NULL,
   PRIMARY KEY (`user_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `okapi_users`.`gostado`
+-- Table `okapi_users`.`like`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `okapi_users`.`gostado` (
+CREATE TABLE IF NOT EXISTS `okapi_users`.`like` (
   `user_id` INT NOT NULL,
   `item_id` INT NOT NULL,
   PRIMARY KEY (`user_id`, `item_id`),
-  CONSTRAINT `fk_gostado_1`
+  CONSTRAINT `fk_like_1`
     FOREIGN KEY (`user_id`)
     REFERENCES `okapi_users`.`user` (`user_id`)
     ON DELETE NO ACTION
@@ -44,52 +44,52 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `okapi_users`.`user_tag`
+-- Table `okapi_users`.`tag`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `okapi_users`.`user_tag` (
+CREATE TABLE IF NOT EXISTS `okapi_users`.`tag` (
   `tag_id` INT NOT NULL,
-  `descricao` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`tag_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `okapi_users`.`interesse`
+-- Table `okapi_users`.`interest`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `okapi_users`.`interesse` (
+CREATE TABLE IF NOT EXISTS `okapi_users`.`interest` (
   `id` INT NOT NULL,
   `user_id` INT NOT NULL,
   `tag_id` INT NOT NULL,
-  `fator` DECIMAL NOT NULL,
+  `weight` DECIMAL NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_user_tag_1_idx` (`user_id` ASC),
-  INDEX `fk_user_tag_2_idx` (`tag_id` ASC),
-  CONSTRAINT `fk_user_tag_1`
+  INDEX `fk_tag_1_idx` (`user_id` ASC),
+  INDEX `fk_tag_2_idx` (`tag_id` ASC),
+  CONSTRAINT `fk_tag_1`
     FOREIGN KEY (`user_id`)
     REFERENCES `okapi_users`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_tag_2`
+  CONSTRAINT `fk_tag_2`
     FOREIGN KEY (`tag_id`)
-    REFERENCES `okapi_users`.`user_tag` (`tag_id`)
+    REFERENCES `okapi_users`.`tag` (`tag_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `okapi_users`.`medidas_corpo`
+-- Table `okapi_users`.`body_measurement`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `okapi_users`.`medidas_corpo` (
+CREATE TABLE IF NOT EXISTS `okapi_users`.`body_measurement` (
   `id` INT NOT NULL,
   `user_id` INT NULL,
-  `peito` INT NULL,
-  `cintura` INT NULL,
-  `anca` INT NULL,
-  `calcado` INT NULL,
+  `chest` INT NULL,
+  `waist` INT NULL,
+  `hips` INT NULL,
+  `foot_size` INT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_medidas_corpo_1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_medidas_corpo_1`
+  INDEX `fk_body_measurement_1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_body_measurement_1`
     FOREIGN KEY (`user_id`)
     REFERENCES `okapi_users`.`user` (`user_id`)
     ON DELETE NO ACTION
