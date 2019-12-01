afefcd214bd01366233583f2cfbd9884743b76ae
Rui Calheno <a78085@alunos.uminho.pt>
2019-12-01 17:02:40 +0000

    Added custom authentication error  message

---
+++ user_management_service/user_management_api/start/hooks.js
@@ -0,0 +1,11 @@
+const { hooks } = require('@adonisjs/ignitor')
+
+hooks.after.providersBooted(() => {
+  const Exception = use('Exception')
+
+  Exception.handle('InvalidJwtToken', async (error, { response}) => {
+    return response.status(401).json({
+        message: 'Unauthorized.'    
+    })
+  })
+})
