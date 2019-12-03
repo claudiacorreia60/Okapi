fd74c2130a916d13829b4ac59bf34721eccbe88f
Armando Santos <armandoifsantos@gmail.com>
2019-12-03 16:45:26 +0000

    Partnership page

--- okapi-frontend/src/router/index.js
+++ okapi-frontend/src/router/index.js
@@ -24,6 +24,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/partnerships',
+    name: 'partnerships',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Partnerships.vue'),
+  },
 ];
 
 const router = new VueRouter({
