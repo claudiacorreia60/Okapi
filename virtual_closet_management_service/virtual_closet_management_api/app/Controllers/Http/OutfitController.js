7616b9cb0a63633345f0c3789132198667fa90ce
Rui Calheno <a78085@alunos.uminho.pt>
2019-12-04 15:38:00 +0000

    Fix addOutfit controller

--- virtual_closet_management_service/virtual_closet_management_api/app/Controllers/Http/OutfitController.js
+++ virtual_closet_management_service/virtual_closet_management_api/app/Controllers/Http/OutfitController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Outfit = use('App/Models/Outfit')
+const OutfitItem = use('App/Models/OutfitItem')
 
 class OutfitController {
     async indexByUser ({request, response, params: {user_id}}) {
@@ -21,11 +22,40 @@ class OutfitController {
         outfit['user_id'] = user_id
         outfit['outfit_id'] = null
 
-        // TODO: how to do this?
+        await outfit.save()
+                    .then(async data => {
 
-        return response.json({
-            message: 'Outfit successfully saved.'
-        })
+                        /* Get last inserted outfit */
+                        outfit = await Outfit.query()
+                             .where('user_id', user_id)
+                             .max('outfit_id')
+
+                        /* Last inserted outfit id */
+                        var outfit_id = outfit[0]['max(`outfit_id`)'] 
+
+                        /* Parse string to list */
+                        var items = JSON.parse(request.post().items)
+
+                        /* Add outfit items */
+                        items.forEach(async (item_id) => {
+                            var outfititem = new OutfitItem() 
+                            outfititem['outfit_id'] = outfit_id
+                            outfititem['item_id'] = item_id
+
+                            await outfititem.save()
+                        })
+
+                        return response.json({
+                            message: 'Outfit successfully saved.'
+                        })
+                    })
+                    .catch(err => {
+
+                        return response.json({
+                            message: 'Outfit could not be saved.',
+                            err: err
+                        })
+                    })
 
     }
 
