b6acee92463171d2a6070be4c84df24fe50ab037
Rafael Magalhaes <rafaelmagftw@gmail.com>
2020-01-04 18:44:38 +0000

    Implementado carregamento e sugestão com o modelo

--- adviser-service/classifier/outfit_classifier.py
+++ adviser-service/classifier/outfit_classifier.py
@@ -3,7 +3,10 @@ import random
 import requests
 import json
 import os
-#from tensorflow.keras.models import load_model
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
 from data.definitions import Outfit, ClothList
 from data.db_access import Database
 
@@ -13,13 +16,15 @@ class OutfitSugestor:
     def __init__(self):
         # TODO 
         # Carregar o modelo de classificação previamente treinado 
-        # self.model = load_model('model.h5')
+        self.model = load_model('prototype_cnn.h5')
+        print("Loaded model")
 
         self.db = Database()
 
 
     def sugest_outfit(self, user_id: int):
 
+        print("Starting sugestion")
         search_space = self.build_search_space(user_id)
         outfits = self.build_outfits(search_space, 4)
         scores = self.evaluate_outfits(outfits)
@@ -33,17 +38,36 @@ class OutfitSugestor:
         
         return best_outfit
 
+    def loadImage(self, image_path):
+        #print(image_path)
+        if image_path != '':
+            bgr_img = cv2.imread(image_path)
+            bgr_img = cv2.resize(bgr_img, (64,64))
+        else:
+            bgr_img = np.zeros((64,64, 3), dtype="uint8")
+        return bgr_img
+
     def classify_outfit(self, outfit: Outfit) -> float:
         self.download_image(outfit['upper_in']['img_url'], 'upper_in')
         self.download_image(outfit['upper_out']['img_url'], 'upper_out')
         self.download_image(outfit['bottom']['img_url'], 'bottom')
         self.download_image(outfit['feet']['img_url'], 'feet')
 
+        upper_in = self.loadImage('upper_in')
+        upper_out = self.loadImage('upper_out')
+        bottom = self.loadImage('bottom')
+        feet = self.loadImage('feet')
+
+        upper_in = upper_in / 255
+        upper_out = upper_out / 255
+        bottom = bottom / 255
+        feet = feet / 255
+
         # TODO
         # Fazer previsão com o modelo
-        # score = self.model.predict('upper_in', 'upper_out, ...) Passar aqui as imagens!!!
+        score = self.model.predict(upper_in, upper_out, bottom, feet) 
 
-        score = round(random.random(),2)
+        # score = round(random.random(),2)
         outfit['score'] = score
 
         os.remove('upper_in')
@@ -62,6 +86,7 @@ class OutfitSugestor:
         catalogue_num = 1
         search_space = {"upper_in":[],"upper_out":[],"bottom":[],"feet":[]}
 
+        print(user_id)
         user = self.db.get_user(user_id=user_id)
         
         likes_upper_in = user['likes']['upper_in']
