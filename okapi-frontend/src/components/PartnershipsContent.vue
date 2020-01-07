99f6767189c7dd9226cf070b632208ea3edb17c1
Cláudia Correia <a77431@alunos.uminho.pt>
2020-01-07 12:11:52 +0000

    profiling não acabado

--- okapi-frontend/src/components/PartnershipsContent.vue
+++ okapi-frontend/src/components/PartnershipsContent.vue
@@ -8,16 +8,27 @@
         header="Find our top tier partners."
     >
     </b-jumbotron>
-    <b-container fluid>
+    <b-container>
       <b-row align-v="center" class="text-center">
-        <b-col sm="12" md="4" id="lionofporches" class="category" align-self="center">
-              <img src="../assets/lionofporches.png" class="category"/>
+        <b-col sm="12" md="6" id="lionofporches" class="category" align-self="center">
+          <a href="https://www.lionofporches.pt/">
+            <img src="../assets/lionofporches.png" class="category"/>
+          </a>
         </b-col>
-        <b-col sm="12" md="4" id="decenio" class="category ml-auto mr-auto" align-self="center">
-              <img src="../assets/decenio.jpg" class="category"/>
+        <b-col sm="12" md="6" id="decenio" class="category ml-auto mr-auto" align-self="center">
+          <a href="https://www.decenio.com/">
+            <img src="../assets/decenio.jpg" class="category"/>
+          </a>
         </b-col>
-        <b-col sm="12" md="4" id="anasousa" class="category" align-self="center">
-              <img src="../assets/anasousa.png" class="category"/>
+        <b-col sm="12" md="6" id="anasousa" class="category" align-self="center">
+          <a href="https://www.temperaturaanasousa.com/">
+            <img src="../assets/anasousa.png" class="category"/>
+          </a>
+        </b-col>
+        <b-col sm="12" md="6" id="mingalondon" class="category mb-5" align-self="center">
+          <a href="https://www.mingalondon.com/">
+            <img src="../assets/mingalondon.png" class="category"/>
+          </a>
         </b-col>
       </b-row>
     </b-container>
--- okapi-frontend/src/views/Profiling.vue
+++ okapi-frontend/src/views/Profiling.vue
@@ -1,42 +1,40 @@
 <template>
-  <b-container>
-    <b-row align-v="center" align-h="center">
-        <h2> Tell us what's your style </h2>
+  <b-container fluid>
+    <b-row align-v="center" align-h="center" class="margin-side">
+        <div class="title mb-5 mt-3">TELL US WHAT YOU LIKE!</div>
         <b-card-group>
-        <b-col md="2" sm="6" align-self="center" v-for="item in items"
+        <b-col md="2" sm="6" align-self="center" class="mt-5" v-for="item in items"
         v-bind:key="item.item_id">
-                <b-card
-                    :img-src="item.photo"
-                    img-top
-                    :alt="item.reference"
-                    class="text-center mb-2"
-                    :id="item.item_id"
-                >
-                    <a id="like-blue"
-                    v-if="(!clickedLike[item.item_id])"
-                    href=""
-                    v-on:click="clickLikeBlue($event, item.item_id)"
-                    >
-                        <unicon name="thumbs-up" class="mr-2" fill="#999999" />
-                    </a>
-                    <a id="like-green" v-else>
-                        <unicon name="thumbs-up" class="mr-2" fill="#6D3C1D" />
-                    </a>
-                    <a id="dislike-blue"
-                    v-if="(!clickedDislike[item.item_id])"
-                    href=""
-                    v-on:click="clickDislikeBlue($event, item.item_id)"
-                    >
-                        <unicon name="thumbs-down" class="ml-2" fill="#999999" />
-                    </a>
-                    <a id="dislike-red" v-else>
-                        <unicon name="thumbs-down" class="ml-2" fill="#6D3C1D" />
-                    </a>
-                </b-card>
+          <b-card
+            v-if="items_like[item.item_id]"
+            :img-src="item.photo"
+            img-top
+            :alt="item.reference"
+            class="text-center mb-2 like"
+            :id="item.item_id"
+            @click="like(item.item_id)"
+          >
+            <div class="middle">
+              <unicon name="heart" class="like-icon" fill="white"></unicon>
+              LIKE
+            </div>
+          </b-card>
+          <b-card
+            v-else
+            class="text-center mb-2 dislike"
+            :id="item.item_id"
+            @click="dislike(item.item_id)"
+          >
+            <b-card-img :src="item.photo" :alt="item.reference"></b-card-img>
+            <div class="middle">
+              <unicon name="heart" class="like-icon" fill="white"></unicon>
+              LIKE
+            </div>
+          </b-card>
         </b-col>
         </b-card-group>
     </b-row>
-    <b-row align-v="center" align-h="center" class="mt-2">
+    <b-row align-v="center" align-h="center" class="mt-5">
         <b-button @click="$router.push('catalog/man')" class="signin-button" type="submit">Proceed</b-button>
     </b-row>
   </b-container>
@@ -53,8 +51,7 @@ export default {
   data() {
     return {
       items: [],
-      clickedLike: {},
-      clickedDislike: {},
+      items_like: {},
     };
   },
   mounted() {
@@ -66,77 +63,117 @@ export default {
         }
     })
     .then(r => r.json())
-    .then(r => this.items = r.slice(0,6))
+    .then(r => { this.items = r.slice(0,6);
+                  this.fetch2();})
     .catch(err => console.log(err));
-
-    fetch("http://localhost:3333/catalog/man?color=Amarelo", {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    })
-    .then(r => r.json())
-    .then(r => this.items = this.items.concat(r.slice(0,6)))
-    .catch(err => console.log(err));
-
-    fetch("http://localhost:3333/catalog/man?color=Vermelho&type=calças e calçoes", {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    })
-    .then(r => r.json())
-    .then(r => this.items = this.items.concat(r.slice(0,6)))
-    .catch(err => console.log(err));
-
-    fetch("http://localhost:3333/catalog/man?color=Castanho&type=calçado", {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
-        }
-    })
-    .then(r => r.json())
-    .then(r => { this.items = this.items.concat(r.slice(0,6)); this.$forceUpdate(); })
-    .catch(err => console.log(err));
-
-    this.clickedLike = cloths.cloths.reduce((acc, cur) => {
-      acc[cur.item_id] = false;
-      return acc;
-    }, {});
-
-    this.clickedDislike = cloths.cloths.reduce((acc, cur) => {
+    
+    this.items_like = this.items.reduce((acc, cur) => {
       acc[cur.item_id] = false;
       return acc;
     }, {});
   },
   methods: {
-    clickLikeBlue(e, id) {
-      e.preventDefault();
-      this.clickedLike[id] = true;
-      // Chamada a API
-
-      this.clickedDislike[id] = false;
-      this.$forceUpdate();
+    like(id) {
+      this.items_like[id] = true;
+      console.log('like');
     },
-    clickDislikeBlue(e, id) {
-      e.preventDefault();
-      this.clickedDislike[id] = true;
-      // Chamada a API
-
-      this.clickedLike[id] = false;
-      this.$forceUpdate();
+    dislike(id) {
+      this.items_like[id] = false;
+      console.log('dislike');
     },
+    fetch2() {
+      fetch("http://localhost:3333/catalog/man?color[=Amarelo]", {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+      })
+      .then(r => r.json())
+      .then(r => { this.items = this.items.concat(r.slice(0,6));
+                   fetch3();})
+      .catch(err => console.log(err));
+    },
+    fetch3() {
+      fetch("http://localhost:3333/catalog/man?color=Vermelho&type[=calças e calçoes]", {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+      })
+      .then(r => r.json())
+      .then(r => { this.items = this.items.concat(r.slice(0,6));
+                    fetch4();})
+      .catch(err => console.log(err));
+    },
+    fetch4() {
+      fetch("http://localhost:3333/catalog/man?color=Castanho&type[=calçado]", {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+      })
+      .then(r => r.json())
+      .then(r => { this.items = this.items.concat(r.slice(0,6)); this.$forceUpdate(); })
+      .catch(err => console.log(err));
+    }
   },
 };
 </script>
 
 <style lang="scss">
+.like-icon {
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+
+.card:hover img {
+  filter: brightness(0.70);
+}
+
+.card:hover .middle {
+  opacity: 1;
+}
+
+.like img {
+  filter: brightness(0.70);
+}
+
+.like .middle {
+  opacity: 1;
+}
+
+.card {
+  border: none;
+  cursor: pointer;
+}
+
+.title {
+  color: #2B1E02;
+  font-size: 20px;
+}
+
+.margin-side {
+  margin-left: 10%;
+  margin-right: 10%;
+  margin-top: 5%;
+}
 
 .signin-button {
   margin-top: 20px;
   margin-bottom: 60px;
   background-color: #2B1E02;
-  width: 100%;
+  width: 170px;
 }
 
 .signin-button:hover {
